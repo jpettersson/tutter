f93d48b7301ba1eccc0353d1328803898bd8d45e
Jonathan Pettersson <jonathan.anders.pettersson@gmail.com>
2014-07-07 13:16:42 +0200

    Checking the x-github-event header for ping events

--- lib/tutter.rb
+++ lib/tutter.rb
@@ -31,23 +31,7 @@ class Tutter < Sinatra::Base
     false
   end
 
-  post '/' do
-    # Github send data in JSON format, parse it!
-    begin
-      data = JSON.parse request.body.read
-    rescue JSON::ParserError
-      error(400, 'POST data is not JSON')
-    end
-
-    # Get a 200 OK message in the webhook history
-    if data['hook']
-      return 200, "Tutter likes this hook!"
-    end
-
-    unless data['repository']
-      error(500, 'Invalid request')
-    end
-
+  def try_action data
     project = data['repository']['full_name'] || error(400, 'Bad request')
 
     conf = get_project_settings(project) || error(404, 'Project does not exist in tutter.conf')
@@ -68,7 +52,36 @@ class Tutter < Sinatra::Base
                                  data)
 
     status_code, message = action.run
+
     return status_code, message
+  end
+
+  post '/' do
+    event = env['HTTP_X_GITHUB_EVENT']
+
+    unless event
+      error(500, 'Invalid request')
+    end
+
+    # Get a 200 OK message in the webhook history
+    if event == "ping"
+      return 200, "Tutter likes this hook!"
+    end
+
+    # Github send data in JSON format, parse it!
+    begin
+      data = JSON.parse request.body.read
+    rescue JSON::ParserError
+      error(400, 'POST data is not JSON')
+    end
+
+    # Check actions with repo
+
+    if data['repository']
+      return try_action(data)
+    end
+
+    error(400, "Unsupported request: #{event}")
   end
 
   get '/' do
